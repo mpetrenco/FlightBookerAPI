908f4e829bc5b1c50459ba2432d927a32e57f51b
Mihai Petrenco <Mihai.Petrenco@endava.com>
2021-04-20 12:30:43 +0300

    Added initial project structure

---
+++ Sources/App/Models/Flight.swift
@@ -0,0 +1,10 @@
+//
+//  Flight.swift
+//  Created by Mihai Petrenco on 4/20/21.
+//
+
+import Foundation
+
+final class Flight {
+    
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,9 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
+    
+    app.get { _ in
+        return "Hello!"
     }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
+    
 }
