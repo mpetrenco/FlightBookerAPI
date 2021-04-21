36e2321e0c177fb4a3e06e455c6f498d555215da
Mihai Petrenco <Mihai.Petrenco@endava.com>
2021-04-22 00:13:22 +0300

    Configured Heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,11 +7,20 @@ public func configure(_ app: Application) throws {
     /**
      * Configure the database driver to be used.
      */
-    app.databases.use(.postgres(hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-                                port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-                                username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-                                password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-                                database: Environment.get("DATABASE_NAME") ?? "vapor_database"), as: .psql)
+    if var config = Environment.get("DATABASE_URL")
+        .flatMap(URL.init)
+        .flatMap(PostgresConfiguration.init) {
+        
+        config.tlsConfiguration = .forClient(certificateVerification: .none)
+        app.databases.use(.postgres(configuration: config), as: .psql)
+        
+    } else {
+        app.databases.use(.postgres(hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+                                    port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+                                    username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+                                    password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+                                    database: Environment.get("DATABASE_NAME") ?? "vapor_database"), as: .psql)
+    }
     
     /**
      * Add the schemas to the database.
